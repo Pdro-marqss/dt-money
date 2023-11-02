b408c2deb52c19bbc6cda1dbda845afba069feb4
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-01 21:57:17 -0300

    feat: summaryComponent created

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { SummaryContainer, SummaryCard } from "./styles";
+
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
+
+export function Summary() {
+   return (
+      <SummaryContainer>
+         <SummaryCard>
+            <header>
+               <span>Entradas</span>
+               <ArrowCircleUp size={32} color="#00B37E" />
+            </header>
+
+            <strong>R$ 17.400,00</strong>
+         </SummaryCard>
+
+         <SummaryCard>
+            <header>
+               <span>Saídas</span>
+               <ArrowCircleDown size={32} color="#F75A68" />
+            </header>
+
+            <strong>R$ 17.400,00</strong>
+         </SummaryCard>
+
+         <SummaryCard variant='green'>
+            <header>
+               <span>Total</span>
+               <CurrencyDollar size={32} color="#FFF" />
+            </header>
+
+            <strong>R$ 17.400,00</strong>
+         </SummaryCard>
+      </SummaryContainer>
+   );
+}
