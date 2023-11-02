23e4202012532df36692fc642948971d05662da7
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-02 18:47:27 -0300

    feat: transactionsTable added

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,34 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles";
+
 export function Transactions() {
    return (
       <div>
          <Header />
          <Summary />
+
+         <TransactionsContainer>
+            <TransactionsTable>
+               <tbody>
+                  <tr>
+                     <td>Desenvolvimento de site</td>
+                     <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+                     <td>Venda</td>
+                     <td>13/04/2022</td>
+                  </tr>
+
+                  <tr>
+                     <td>MC Donnalds</td>
+                     <td><PriceHighlight variant="outcome">- R$ 54,00</PriceHighlight></td>
+                     <td>Alimentação</td>
+                     <td>14/04/2022</td>
+                  </tr>
+
+               </tbody>
+            </TransactionsTable>
+         </TransactionsContainer>
       </div>
    );
 }
