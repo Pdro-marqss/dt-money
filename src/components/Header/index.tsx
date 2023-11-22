85a57c7267fa90fc2ed1818151fbbdf1574a6788
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-22 19:30:12 -0300

    feat: install Radix and use Dialog component with acessiblity rules

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import * as Dialog from '@radix-ui/react-dialog';
+
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
@@ -8,7 +10,22 @@ export function Header() {
          <HeaderContent>
             <img src={logoImg} alt="" />
 
-            <NewTransactionButton>Nova transação</NewTransactionButton>
+            <Dialog.Root>
+               <Dialog.Trigger asChild>
+                  <NewTransactionButton>Nova transação</NewTransactionButton>
+               </Dialog.Trigger>
+
+               <Dialog.Portal>
+                  <Dialog.Overlay />
+
+                  <Dialog.Content>
+                     <Dialog.Title>Nova transação</Dialog.Title>
+
+                     <Dialog.Close />
+                  </Dialog.Content>
+               </Dialog.Portal>
+            </Dialog.Root>
+
          </HeaderContent>
       </HeaderContainer>
    );
