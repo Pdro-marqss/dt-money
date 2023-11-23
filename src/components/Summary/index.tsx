fcaff7224e83016a98fe1a844bb9bed3c42ab247
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-23 20:26:21 -0300

    feat: using reduce in transactions data to display income, outcome and total on summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,6 +7,26 @@ import { SummaryContainer, SummaryCard } from "./styles";
 export function Summary() {
    const { transactions } = useContext(TransactionsContext);
 
+   const summary = transactions.reduce(
+      (acc, transaction) => {
+         if (transaction.type === 'income') {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+         } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+         }
+
+
+         return acc;
+      },
+      {
+         income: 0,
+         outcome: 0,
+         total: 0
+      }
+   )
+
    return (
       <SummaryContainer>
          <SummaryCard>
@@ -15,7 +35,7 @@ export function Summary() {
                <ArrowCircleUp size={32} color="#00B37E" />
             </header>
 
-            <strong>R$ 17.400,00</strong>
+            <strong>R$ {summary.income}</strong>
          </SummaryCard>
 
          <SummaryCard>
@@ -24,7 +44,7 @@ export function Summary() {
                <ArrowCircleDown size={32} color="#F75A68" />
             </header>
 
-            <strong>R$ 17.400,00</strong>
+            <strong>R$ {summary.outcome}</strong>
          </SummaryCard>
 
          <SummaryCard variant='green'>
@@ -33,7 +53,7 @@ export function Summary() {
                <CurrencyDollar size={32} color="#FFF" />
             </header>
 
-            <strong>R$ 17.400,00</strong>
+            <strong>R$ {summary.total}</strong>
          </SummaryCard>
       </SummaryContainer>
    );
