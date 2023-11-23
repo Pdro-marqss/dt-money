b43d33b576d94069c84a0f3b8eb163e8914a98dd
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-23 01:19:55 -0300

    feat: ModalComponent + styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { NewTransactionModal } from "../NewTransactionModal";
+
 import logoImg from '../../assets/logo.svg';
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export function Header() {
    return (
@@ -15,15 +17,7 @@ export function Header() {
                   <NewTransactionButton>Nova transação</NewTransactionButton>
                </Dialog.Trigger>
 
-               <Dialog.Portal>
-                  <Dialog.Overlay />
-
-                  <Dialog.Content>
-                     <Dialog.Title>Nova transação</Dialog.Title>
-
-                     <Dialog.Close />
-                  </Dialog.Content>
-               </Dialog.Portal>
+               <NewTransactionModal />
             </Dialog.Root>
 
          </HeaderContent>
