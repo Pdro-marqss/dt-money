b35d8be1cd4210c1bfc15c23ba9b75c9ad1a5783
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-11-27 23:53:01 -0300

    feat: Search filter transaction

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
+
 import { useForm } from "react-hook-form";
 import { MagnifyingGlass } from "phosphor-react";
 
@@ -13,6 +16,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+   const { fetchTransactions } = useContext(TransactionsContext);
+
    const {
       register,
       handleSubmit,
@@ -22,8 +27,7 @@ export function SearchForm() {
    });
 
    async function handleSearchTransactions(data: SearchFormInputs) {
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      console.log(data);
+      await fetchTransactions(data.query);
    }
 
    return (
